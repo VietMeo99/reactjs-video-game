f85d13bdb73a303423ec8abaf8ca04f528063259
V_Dev99 <vuhoangviet160799@gmail.com>
2021-02-17 11:08:16 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
       App
-    </div>
+    </div> 
   );
-}
+};
 
 export default App;
